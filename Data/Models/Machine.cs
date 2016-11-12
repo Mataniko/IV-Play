ff1b84767f33f835e3796cb8db123f46b044c23d
Matan Bareket <mataniko@gmail.com>
2016-11-12 13:39:18 -0500

    Set name to index

--- Data/Models/Machine.cs
+++ Data/Models/Machine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace IV_Play.Data.Models
     public class Machine
     {
         [XmlAttribute]
+        [BsonIndex(true)]
         public string name { get; set; }
         [XmlAttribute]
         public string sourcefile { get; set; }
