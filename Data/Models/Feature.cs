a2146a4a2c68902f87687f1929f4b964db7b6902
Matan Bareket <mataniko@gmail.com>
2017-09-02 17:36:56 -0400

    Add Feature model
    Feature elements were added in Mame 189

---
+++ Data/Models/Feature.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace IV_Play.Data.Models
+{
+    public class Feature
+    {
+        [XmlAttribute]
+        public string type { get; set; }
+
+        [XmlAttribute]
+        public string status { get; set; }
+
+        [XmlAttribute]
+        public string overall { get; set; }
+    }
+}
