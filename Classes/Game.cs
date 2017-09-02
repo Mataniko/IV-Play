46164b3f025aceadc0b1b8d4093bb85b3be66c56
Matan Bareket <mataniko@gmail.com>
2017-09-02 17:37:36 -0400

    Support Feature elements and move string parsing methods from the Model

--- Classes/Game.cs
+++ Classes/Game.cs
@@ -2,9 +2,11 @@
 
 using IV_Play.Data.Models;
 using IV_Play.Properties;
+using System.Linq;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Text;
 
 #endregion
 
@@ -28,22 +30,23 @@ namespace IV_Play
             Children = new SortedList<string, Game>();
             IsFavorite = false;
             CloneOf = string.IsNullOrEmpty(machine.cloneof) ? machine.name : machine.cloneof;
-            CPU = machine.cpuinfo();
+            CPU = ParseCpu(machine.chip);
             Description = machine.description;
             SourceFile = machine.sourcefile;
             Name = machine.name;
             Manufacturer = machine.manufacturer;
             ParentSet = machine.cloneof;
-            Screen = machine.displayinfo();
-            Sound = machine.soundinfo();
+            Screen = ParseDisplay(machine.display);
+            Sound = ParseSound(machine.chip, machine.sound);
             Working = machine.driver != null ? machine.driver.emulation == "good" : true;
             Year = machine.year;
             IconPath = Settings.Default.icons_directory + machine.name + ".ico";
-            Driver = machine.driver != null ? machine.driver.ToString() : null;
+            Driver = machine.driver != null ? machine.driver.ToString() : null;          
             Input = machine.input != null ? machine.input.ToString() : null;
-            Display = machine.displayinfo();
+            Features = ParseFeatures(machine.feature);
+            Display = ParseDisplay(machine.display);
             //Colors = colors, Doesn't exist anymore?
-            Roms = machine.rominfo();
+            Roms = ParseRom(machine.rom, machine.disk);
             IsMechanical = machine.ismechanical == "yes";
             ShowAsParent = false;
         }
@@ -69,6 +72,7 @@ namespace IV_Play
         public string Colors { get; set; }
         public string History { get; set; }
         public string Info { get; set; }
+        public string Features { get; set; }
         public bool HasOverlay { get; set; }
         public bool IsMechanical { get; set; }
         public bool ShowAsParent { get; set; }
@@ -88,6 +92,65 @@ namespace IV_Play
         public override string ToString()
         {
             return Name;
+        }
+
+        private string ParseFeatures(Feature[] feature)
+        {
+            if (feature == null) return "";
+
+            var sb = new StringBuilder();            
+            foreach (var obj in feature)
+            {
+                var status = "";
+                if (!String.IsNullOrEmpty(obj.status) && !String.IsNullOrEmpty(obj.overall))
+                    status = string.Format("{0};{1}", obj.status, obj.overall);
+                else if (String.IsNullOrEmpty(obj.status))
+                    status = obj.overall;
+                else
+                    status = obj.status;
+                sb.Append(string.Format("{0}={1}, ", System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(obj.type), status));
+            }
+
+            return sb.ToString().TrimEnd(new char[2] { ',', ' '});
+        }
+        private string ParseCpu(Chip[] chip)
+        {
+            if (chip == null) return string.Empty;
+            return GetStringFromArray(chip.Where(x => x.type == "cpu").ToArray());
+        }
+
+        private string ParseRom(Rom[] rom, Disk[] disk)
+        {
+            var roms = GetStringFromArray(rom);
+            var disks = GetStringFromArray(disk);
+
+            return roms + "\r\n" + disks;
+        }
+
+        private string ParseDisplay(Display[] display)
+        {
+            return GetStringFromArray(display);
+        }
+
+        private string ParseSound(Chip[] chip, Sound sound)
+        {
+            if (chip == null || sound == null) return string.Empty;
+            var soundString = GetStringFromArray(chip.Where(x => x.type == "audio").ToArray());
+            return string.Format("{0} Channel(s)\r\n{1}", sound.channels, soundString);
+        }
+
+        private string GetStringFromArray(object[] array)
+        {
+            if (array == null) return string.Empty;
+
+            var sb = new StringBuilder();
+
+            foreach (var obj in array)
+            {
+                sb.AppendLine(obj.ToString());
+            }
+
+            return sb.ToString();
         }
     }
 
